d0822c132e1797c6a7102b2c0a6676b893213b65
aismai <abai.bakytbek@gmail.com>
2020-12-01 20:41:05 +0600

    create app component

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "marat",
+          name: "Marat Kulbaev",
+          phone: "+996333444555",
+          email: "marat@xmpl.com",
+        },
+        {
+          id: "meerim",
+          name: "Meerim Tokocheva",
+          phone: "+996333444666",
+          email: "meepum@xmpl.com",
+        },
+      ],
+    }
+  },
+}
+</script>
