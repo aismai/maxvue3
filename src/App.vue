82884b1ae000c2c2233a22cb79e439d3883c1397
aismai <abai.bakytbek@gmail.com>
2020-12-02 11:37:04 +0600

    add friends component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <section>
-    <h2>My Friends</h2>
+    <header><h1>My Friends</h1></header>
     <ul>
-      <li></li>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :id="friend.id"
+        :name="friend.name"
+        :email="friend.email"
+        :phone-number="friend.phone"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
+      ></friend-contact>
     </ul>
   </section>
 </template>
@@ -17,15 +26,88 @@ export default {
           name: "Marat Kulbaev",
           phone: "+996333444555",
           email: "marat@xmpl.com",
+          isFavorite: true,
         },
         {
           id: "meerim",
           name: "Meerim Tokocheva",
           phone: "+996333444666",
           email: "meepum@xmpl.com",
+          isFavorite: false,
         },
       ],
     }
   },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const friend = this.friends.find((friend) => friend.id === friendId)
+      friend.isFavorite = !friend.isFavorite
+    },
+  },
 }
 </script>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,65 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide details" : "Show details" }}
+    </button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  emits: ["toggle-favorite"],
+  //   emits: {
+  //     "toggle-favorite": function(id) {
+  //       if (id) {
+  //         return true
+  //       } else {
+  //         console.warn("id is missing")
+  //         return false
+  //       }
+  //     },
+  //   },
+  data() {
+    return {
+      detailsAreVisible: false,
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id)
+    },
+  },
+}
+</script>
